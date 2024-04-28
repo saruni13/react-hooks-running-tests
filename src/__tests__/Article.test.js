ee49ad7f4ce1384577bc18f765f0a43c83bcd570
saruni <timothysaruni13@gmail.com>
2024-04-28 12:30:45 +0300

    adds complete lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,15 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
-
 import Article from "../components/Article";
-
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element)
+
+
+  expect(element).toBeInTheDocument();
 });
--- src/components/Article.js
+++ src/components/Article.js
@@ -2,10 +2,11 @@ import React from "react";
 
 function Article() {
   return (
-    <div>
-      {/* please pass this test */}
+  <div>
+      please pass this test 
     </div>
-  );
+  )
+  
 }
 
 export default Article;
